15faff814430cda1823009044c9f58b4dd28c7f1
Camila <mariacamilacalle1@gmail.com>
2024-03-29 11:42:17 -0500

    third commit

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -4,102 +4,90 @@ import { useState } from 'react';
 
 
 const Formulario = () => {
+  const [nombre, setNombre] = useState("");
+  const [apellido, setApellido] = useState("");
+  const [contraseña, setContraseña] = useState("");
+  const [confirmarContraseña, setconfirmarContraseña] = useState("");
+  const [error, setError] = useState(false);
+  const [registroExitoso, setRegistroExitoso] = useState(false);
 
-<form>
+  const validarDatos = (e) => {
+    e.preventDefault();
+    //Validación;
+    if (
+      nombre === "" ||
+      apellido === "" ||
+      contraseña === "" ||
+      confirmarContraseña === ""
+    ) {
+      setError(true);
+      setRegistroExitoso(false);
+      return;
+    }
+    setError(false);
+    setRegistroExitoso(true);
+    setNombre("");
+    setApellido("");
+    setContraseña("");
+    setconfirmarContraseña("");
+  };
 
-<div className="form-group">
-         <label className="title_button">Nombre</label>
-          <input type="text" name="nombre" ></input>
+  return (
+    <>
+      <form className="formulario" onSubmit={validarDatos}>
+        {error ? <p className="wrong">Los campos están vacios</p> : null}
+        {registroExitoso && <p className="exito">Registro exitoso</p>}
 
- </div>
-</form>
-}
+        <div className="form-group">
+          <label className="title_button">Nombre</label>
+          <input
+            type="text"
+            name="nombre"
+            className="form-control"
+            onChange={(e) => setNombre(e.target.value)}
+            value={nombre}
+          />
+        </div>
+
+        <div className="form-group">
+          <label className="title_button">Apellido</label>
+          <input
+            type="text"
+            name="apellido"
+            className="form-control"
+            onChange={(e) => setApellido(e.target.value)}
+            value={apellido}
+          />
+        </div>
+
+        <div className="form-group">
+          <label className="title_button">Contraseña</label>
+          <input
+            type="password"
+            name="contraseña"
+            className="form-control"
+            onChange={(e) => setContraseña(e.target.value)}
+            value={contraseña}
+          />
+        </div>
+
+        <div className="form-group">
+          <label className="title_button">Repetir Contraseña</label>
+          <input
+            type="password"
+            name="confirmar Contraseña"
+            className="form-control"
+            onChange={(e) => setconfirmarContraseña(e.target.value)}
+            value={confirmarContraseña}
+          />
+        </div>
+
+        <button type="submit" className="main_button">
+          Enviar
+        </button>
+      </form>
+    </>
+  );
+};
 
 export default Formulario;
-
-
-// const Formulario = () => {
-
-
-//     const [nombre, setNombre] = useState('');
-//     const [apellido, setApellido] = useState('');
-//     const [contraseña, setContraseña] = useState('');
-//     const [confirmarContraseña, setconfirmarContraseña] = useState('');
-//     const [error, setError] = useState(false);
-//     const [registroExitoso, setRegistroExitoso] = useState(false);
-
-//     const validarDatos = (e) => {
-//         e.preventDefault();
-//         //Validación;
-//         if (nombre === '' || apellido === '' || contraseña === '' || confirmarContraseña === '')
-//         {
-//         setError(true);
-//         setRegistroExitoso(false);
-//         return;
-//         }
-//         setError(false);
-//         setRegistroExitoso(true);
-//         setNombre('');
-//     setApellido('');
-//     setContraseña('');
-//     setconfirmarContraseña('');
-//         };
-
-
-
-
-
-//     return (
-//         <>
-
-// <form className="formulario"
-//       onSubmit={validarDatos}>
-//       {error ? <p className="wrong">Los campos están vacios</p> : null}
-//       {registroExitoso && <p className="exito">Registro exitoso</p>}
-
-//         <div className="form-group">
-//           <label className="title_button">Nombre</label>
-//           <input type="text" name="nombre" className="form-control" onChange={(e) => setNombre(e.target.value)}
-// value={nombre} />
-//         </div>
-
-
-//         <div className="form-group">
-//           <label  className="title_button">Apellido</label>
-//           <input type="text" name="apellido" className="form-control"
-//           onChange={(e) => setApellido(e.target.value)}
-//           value={apellido} />
-//         </div>
-
-
-//         <div className="form-group">
-//           <label  className="title_button">Contraseña</label>
-//           <input type="password" name="contraseña" className="form-control"
-//           onChange={(e) => setContraseña(e.target.value)}
-//           value={contraseña} />
-//         </div>
-
-
-//         <div className="form-group">
-//           <label  className="title_button">Repetir Contraseña</label>
-//           <input type="password" name="confirmar Contraseña" className="form-control"
-//           onChange={(e) => setconfirmarContraseña(e.target.value)}
-//           value={confirmarContraseña}/>
-//         </div>
-
-
-//         <button
-//           type="submit"
-//           className="main_button"
-//         >
-//           Enviar
-//         </button>
-//       </form>
-
-//   </>
-
-//     )
-
-// }
-
-// export default Formulario;
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/nombre-de-tu-repositorio/', // Reemplaza 'nombre-de-tu-repositorio' con el nombre real de tu repositorio en GitHub
   plugins: [react()],
-  build: {
-    outDir: 'docs', // Carpeta de salida para GitHub Pages
-  },
-});
+})
